c75619ed3e6af470a51c89d549d28a72c642bc20
Leigh Klotz <klotz@klotz.me>
2021-03-03 20:26:21 -0800

    use podman by default

--- doit.sh
+++ doit.sh
@@ -5,7 +5,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 . ${DIR}/config.sh
 
 # https://medium.com/@suci/running-pyspark-on-jupyter-notebook-with-docker-602b18ac4494
-docker run -it --rm -p 8888:8888 -v "${WORKBOOK} " \
+podman run -it --rm -p 8888:8888 -v "${WORKBOOK} " \
        jupyter/pyspark-notebook 
 
 echo "http://localhost:8888"
