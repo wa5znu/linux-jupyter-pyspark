4710cb72c3450d0b89a9d4653b16889b3099f9cf
Leigh Klotz <klotz@klotz.me>
2021-04-20 14:10:06 -0700

    podman fails except as root on ubuntu

--- doit.sh
+++ doit.sh
@@ -8,7 +8,8 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 chmod -R 777 $WORKBOOK
 
 # https://medium.com/@suci/running-pyspark-on-jupyter-notebook-with-docker-602b18ac4494
-podman run -it --rm -p 8888:8888 -v $WORKBOOK:/home/jovyan/work \
-       jupyter/pyspark-notebook 
+#jupyter/pyspark-notebook 
+sudo podman run -it --rm -p 8888:8888 -v $WORKBOOK:/home/jovyan/work \
+       docker.io/jupyter/pyspark-notebook:latest &&
 
 echo "http://localhost:8888"
