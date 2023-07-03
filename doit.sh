41a13947bd63009b9872d020a181562accbd53c2
Leigh Klotz <klotz@klotz.me>
2023-07-02 18:26:36 -0700

    doit.sh: expand to jupyter/tensorflow and others NOTEBOOK env var controls base image couldn't get gpu-jupyter to work yet

--- doit.sh
+++ doit.sh
@@ -7,9 +7,19 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # podman / docker workaround
 chmod -R 777 $WORKBOOK
 
+# PySpark
 # https://medium.com/@suci/running-pyspark-on-jupyter-notebook-with-docker-602b18ac4494
-#jupyter/pyspark-notebook 
-sudo podman run -it --rm -p 8888:8888 -v $WORKBOOK:/home/jovyan/work \
-       docker.io/jupyter/pyspark-notebook:latest &&
 
-echo "http://localhost:8888"
+# Data Science / TensorFlow
+# jupyter/pyspark-notebook 
+# github: iot-salzburg/gpu-jupyter -> cschranz/gpu-jupyter
+# jupyter/scipy-notebook -> jupyter/datascience-notebook -> jupyter/tensorflow-notebook
+
+NOTEBOOK=${NOTEBOOK:-jupyter/tensorflow-notebook}
+
+echo "notebook=$NOTEBOOK workbook=$WORKBOOK webui=http://localhost:8888 - token=see output"
+
+sudo podman run -it --rm -p 8888:8888 -v $WORKBOOK:/home/jovyan/work \
+       docker://${NOTEBOOK}:latest
+
+
